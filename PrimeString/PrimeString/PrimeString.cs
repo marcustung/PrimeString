2a0d63151c7eed0546e43ed564c893d600a8a196
marcustung <marcustung116@gmail.com>
2017-07-22 18:23:24 +0800

    First Vsersion

---
+++ PrimeString/PrimeString/PrimeString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeString
+{
+	public class PrimeString
+	{
+		public bool isPerminString(string input)
+		{
+			bool isPerminString = true;
+
+			for (int i = 2; i < input.Length; i++)
+			{
+				var oristr = input;
+				var str = oristr.Substring(0, i);
+
+				if ((oristr.Replace(str, string.Empty).Length == 0))
+				{
+					isPerminString = false;
+				}
+			}
+
+			return isPerminString;
+		}
+	}
+}
---
+++ PrimeString/PrimeStringTests/PrimeStringTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using PrimeString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeString.Tests
+{
+	[TestFixture()]
+	public class PrimeStringTests
+	{
+		[Test()]
+		[TestCase("abab", false)]
+		public void isPerminStringTest(string inputValue, bool isPrimeString)
+		{
+			// Arrange
+			PrimeString primeStr = new PrimeString();
+
+			// Act
+			bool result = primeStr.isPerminString(inputValue);
+
+			// Assert
+			Assert.AreEqual(isPrimeString, result);
+		}
+	}
+}
